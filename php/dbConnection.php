6c1d66448608c47204ff5c3daa65e11aae02e49d
fredplatt <fred_platt@hotmail.com>
2019-03-21 15:42:40 +0000

    db name changed to have name, dbConnection changed accordingly

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -1,7 +1,7 @@
 <?php
 
 function getdbConnection() {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=portfolio', 'root');
+    $db = new PDO('mysql:host=127.0.0.1;dbname=fred_portfolio', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
